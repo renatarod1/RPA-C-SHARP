1b413ce0e259034f4edd343eba7ee73fcbd3d347
Renata Rodrigues <ticare1@hotmail.com>
2023-06-25 10:31:32 -0300

    Projeto Simples usando o Selenium

---
+++ SeleniumBot/SeleniumBot/AutomationWeb.cs
@@ -0,0 +1,24 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace SeleniumBot {
+    public class AutomationWeb {
+        public IWebDriver driver;
+
+        public AutomationWeb() {
+            driver = new ChromeDriver();
+        }
+
+        public string TestWeb() {
+            driver.Navigate().GoToUrl("https://www.google.com.br");
+
+            driver.FindElement(By.Name("q")).SendKeys("Site Concatenar");
+            
+            driver.FindElement(By.XPath("/html/body/div[1]/div[3]/form/div[1]/div[1]/div[4]/center/input[1]")).Click();
+            
+            var text = driver.FindElement(By.XPath("//*[@id=\'rso\']/div[1]/div/div/div[2]/div/span")).Text;
+
+            return text;
+        }
+    }
+}
---
+++ SeleniumBot/SeleniumBot/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace SeleniumBot {
+    public class Program {
+        static void Main(string[] args) {
+            var web = new AutomationWeb();
+            var text = web.TestWeb();
+
+            Console.WriteLine(text);
+            Console.ReadLine();
+        }
+    }
+}
